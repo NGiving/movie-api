e78a35cfca9d2d9d02859f8538da7b4783d7dd54
Giving <44300487+NGiving@users.noreply.github.com>
2024-04-01 15:57:23 -0400

    fix

--- app.js
+++ app.js
@@ -8,14 +8,21 @@ app.enable('trust proxy')
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.once('open', () => { console.log('Connected to MongoDB') });
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => {
+    console.log('Hello');
+});
 
 // app.get('/titles', )
 
 app.get('/title/:id', async (req, res) => {
     const { id } = req.params;
-    const title = await Title.find({ show_id: id }).exec();
-    res.json(title);
+    try {
+        const title = await Title.find({ show_id: id }).exec();
+        res.json(title);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error");
+    }
 });
 
 app.listen(process.env.PORT, () => {
