091a06a89cd3a14113910f2826cdc63351cedaf8
Giving <44300487+NGiving@users.noreply.github.com>
2024-04-01 15:06:24 -0400

    added new route

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ const Title = require('./models/title.model');
 app.enable('trust proxy')
 mongoose.connect(process.env.DB_STRING);
 
-app.get('/titles/:id', async (req, res) => {
+app.get('/', (req, res) => res.send('Hello'));
+
+// app.get('/titles', )
+
+app.get('/title/:id', async (req, res) => {
     const { id } = req.params;
     const title = await Title.find({ show_id: id }).exec();
     console.log(title);
     res.json(title);
-})
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
-  })
+});
