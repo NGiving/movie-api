54bfd9fc370631e9831ce36265f193616f43e523
Giving <44300487+NGiving@users.noreply.github.com>
2024-04-07 19:16:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.get('/api/title/:id', async (req, res) => {
 app.get('/api/find/:title', async (req, res) => {
     const title = req.params.title.trim();
     try {
-        const movie = await Title.find({ "title": { $regex: new RegExp(title, "i") } }).sort({ "title": 1 }).limit(10);
+        const movie = await Title.find({ "title": { $regex: new RegExp(title, "i") } }).sort({ "title": -1 }).limit(10);
         res.json(movie)
     } catch (error) {
         console.error(error);
